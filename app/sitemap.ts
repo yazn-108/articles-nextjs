3b5637150ce92addc9a933f1791e84ac787e4691
yazn-108 <gamesalab22@gmail.com>
2025-12-18 18:16:30 +0300

    sitemap + robots

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,19 +3,19 @@ export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
       url: 'https://murakkaz.vercel.app/',
-      lastModified: new Date(),
+      lastModified: new Date('2025-01-01'),
       changeFrequency: 'daily',
       priority: 1,
     },
     {
       url: 'https://murakkaz.vercel.app/about-yazn-108',
-      lastModified: new Date(),
+      lastModified: new Date('2025-01-01'),
       changeFrequency: 'monthly',
       priority: 0.7,
     },
     {
       url: 'https://murakkaz.vercel.app/privacy-policy',
-      lastModified: new Date(),
+      lastModified: new Date('2025-01-01'),
       changeFrequency: 'yearly',
       priority: 0.5,
     },
