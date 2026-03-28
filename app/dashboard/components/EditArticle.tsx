ea23525f52ca6eeec2478134d400aacf6365fc8d
yazn-108 <gamesalab22@gmail.com>
2026-03-28 20:31:30 +0300

    edit article form

--- app/dashboard/components/EditArticle.tsx
+++ app/dashboard/components/EditArticle.tsx
@@ -7,6 +7,7 @@ import { Activity, useState } from "react";
 import { createPortal } from "react-dom";
 import { toast } from "sonner";
 import { Input, Textarea } from "./FormElements";
+import Calendar from "./calendar";
 const EditArticle = ({ article }: { article: ArticleDetailsResponse }) => {
   const [IsOpen, setIsOpen] = useState<boolean>(false);
   const [newArticleLoader, setNewArticleLoader] = useState<boolean>(false);
@@ -46,19 +47,22 @@ const EditArticle = ({ article }: { article: ArticleDetailsResponse }) => {
       title: (data.title as string) ?? article.title,
       tag: (data.tag as string) ?? article.tag,
       description: (data.description as string) ?? article.description,
-      SubscribersNotified: false,
+      createdAt: new Date(data.createdAt as string),
+      SubscribersNotified: data["subscribers-notified"] === "on",
       banner: {
         url: image?.secure_url ?? article.banner.url,
         public_id: image?.public_id ?? article.banner.public_id,
         alt: (data["banner-description"] as string) ?? article.banner.alt,
       },
     };
-    if (JSON.stringify(articleData) !== JSON.stringify(article)) {
-      EditArticleInfo.mutate(articleData);
-    } else {
-      toast.warning("لم يتم إجراء أي تغييرات على المقالة");
-      setNewArticleLoader(false);
-    }
+    console.log(articleData);
+    // if (JSON.stringify(articleData) !== JSON.stringify(article)) {
+    //   EditArticleInfo.mutate(articleData);
+    //   // setIsOpen(false);
+    // } else {
+    //   toast.warning("لم يتم إجراء أي تغييرات على المقالة");
+    //   setNewArticleLoader(false);
+    // }
   };
   return (
     <div>
@@ -112,6 +116,7 @@ const EditArticle = ({ article }: { article: ArticleDetailsResponse }) => {
                     className="mt-4 resize-none h-48"
                     defaultValue={article.description}
                   />
+                  <Calendar name="createdAt" />
                   <Input name="banner" type="file" />
                   <Input
                     name="banner-description"
@@ -119,6 +124,21 @@ const EditArticle = ({ article }: { article: ArticleDetailsResponse }) => {
                     placeholder="وصف الصورة (alt)"
                     defaultValue={article.banner.alt}
                   />
+                  <div>
+                    <input
+                      id="subscribers-notified"
+                      name="subscribers-notified"
+                      type="checkbox"
+                      defaultChecked={article.SubscribersNotified}
+                      className="peer sr-only"
+                    />
+                    <label
+                      htmlFor="subscribers-notified"
+                      className={`cursor-pointer bg-gray-800 px-2 py-1 rounded-md peer-checked:bg-primary ${article.SubscribersNotified && "cursor-not-allowed! bg-gray-600!"}`}
+                    >
+                      إعلام المشتركين عند النشر
+                    </label>
+                  </div>
                   <div>
                     {/* title: string | null;
                       content?: string | null;
